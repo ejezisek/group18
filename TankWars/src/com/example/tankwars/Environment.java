ee8b541dfff10d0aeac909972e9d964e86bf70d3
Edward <ejezisek@knights.ucf.edu>
2013-02-17 03:08:00 -0500

    I added comments to the tank class a few canvas's to the environment class.  I plan on drawing a rough draft of the environment sometime tomorrow.  I will attempt to use OpenGL, but no promises. -Ed

--- TankWars/src/com/example/tankwars/Environment.java
+++ TankWars/src/com/example/tankwars/Environment.java
@@ -1,5 +1,19 @@
 package com.example.tankwars;
 
-public class Environment {
+import android.graphics.Canvas;
+import android.view.View;
+
+public class Environment{
+/////////////////////////////////////////////////////////////////////////
+//initialize variables///////////////////////////////////////////////////
+//The Canvas's allow us to draw different parts of the environment     //
+//separately.                                                          //
+/////////////////////////////////////////////////////////////////////////
+private Canvas drawEnvironment;
+private Canvas drawTank;
+private Canvas drawBullet;
+private Canvas hitTank;
+
+
 
 }
--- TankWars/src/com/example/tankwars/Tank.java
+++ TankWars/src/com/example/tankwars/Tank.java
@@ -1,8 +1,24 @@
 package com.example.tankwars;
 
+//////////////////////////////////////////////////////////////////////////////////////////////
+//Project Name   : Tank Wars                                                                //
+//Author(s)      : Edward Jezisek                                                           //
+//Purpose        : Create a tank class to monitor the tanks location and health.            //
+//Version Number : Version 0.0                                                              //
+//Unit Testing	 : No                                                                       //
+//Date           : 2/12/2013                                                                //
+//Input          : None.                                                                    //
+//Dependencies   : An environment is required for the tank to move around in.               //
+//////////////////////////////////////////////////////////////////////////////////////////////
+//Enhancements	 : First Created Version                                                    //
+//Version x      : No previous Version.                                                     //
+//Date           : None                                                                     //
+//////////////////////////////////////////////////////////////////////////////////////////////
+
 public class Tank {
 	final int MOVE_AMMOUNT=5;
 	final int SCREEN_MAX=1000;
+	final int SCREEN_MIN=0;
 	private int position;
 	private int turret_angle;
 	private int energy;
@@ -18,6 +34,7 @@ public class Tank {
 	//Unit Testing	 : No                                                                       //
 	//Date           : 2/12/2013                                                                //
 	//Input          : isRight, if true, the tank moves right, otherwise it moves left          // 
+	//Dependencies   : MOVE_AMMOUNT, determines how much to move the tank.                      //
 	//////////////////////////////////////////////////////////////////////////////////////////////
 	//Enhancements	 : First Created Version                                                    //
 	//Version x      : No previous Version.                                                     //
@@ -35,14 +52,46 @@ public class Tank {
 		}
 			
 		}
+	
+	//////////////////////////////////////////////////////////////////////////////////////////////
+	//Project Name   : Tank Wars                                                                //
+	//Author(s)      : Edward Jezisek                                                           //
+	//Purpose        : The purpose is to create an environment that the tank can move around in.//
+	//Version Number : Version 0.0                                                              //
+	//Unit Testing	 : No                                                                       //
+	//Date           : 2/12/2013                                                                //
+	//Input          : moveAmmount, decides how much to move the Tank.                          // 
+	//				 : isRight, checks whether the tank moves left or right                     //
+	//Dependencies   : SCREEN_MAX, SCREEN_MIN, Uses these to check if the tanks goes off screen.//
+	//////////////////////////////////////////////////////////////////////////////////////////////
+	//Enhancements	 : First Created Version                                                    //
+	//Version x      : No previous Version.                                                     //
+	//Date           : None                                                                     //
+	//////////////////////////////////////////////////////////////////////////////////////////////
 	boolean canMove(int moveAmmount, boolean isRight)
 	{
 		if(isRight)
 			return (position+moveAmmount<SCREEN_MAX);
 		else
-			return (position-moveAmmount>SCREEN_MAX);
+			return (position-moveAmmount>SCREEN_MIN);
 	}
-	void turret_move(boolean direction)
+	
+	//////////////////////////////////////////////////////////////////////////////////////////////
+	//Project Name   : Tank Wars                                                                //
+	//Author(s)      : Edward Jezisek                                                           //
+	//Purpose        : The purpose is to create an environment that the tank can move around in.//
+	//Version Number : Version 0.0                                                              //
+	//Unit Testing	 : No                                                                       //
+	//Date           : 2/12/2013                                                                //
+	//Input          : moveAmmount, decides how much to move the Turrets angle.                 // 
+	//				 : direction, checks which direction to move the turret.                    //
+	//Dependencies   : None.                                                                    //
+	//////////////////////////////////////////////////////////////////////////////////////////////
+	//Enhancements	 : First Created Version                                                    //
+	//Version x      : No previous Version.                                                     //
+	//Date           : None                                                                     //
+	//////////////////////////////////////////////////////////////////////////////////////////////
+	void turret_move(int moveAmmount, boolean direction)
 		{
 		
 		}
